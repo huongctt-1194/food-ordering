23930535d060124ca64e0b5bb0749a504bf7aea0
huongctt-1149 <chu.thi.thanh.huong@sun-asterisk.com>
2021-10-26 11:11:18 +0700

    edit views and add category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,35 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+      @category = Category.new(category_params)
+      if @category.save
+          flash[:notice] = t 'flash.category.success'
+          redirect_to @category
+      else
+          render 'new'
+      end
+  end
+
+  def show
+    begin
+      @category = Category.find(params[:id])
+    rescue
+      flash[:notice] = t 'flash.category.notfound'
+      redirect_to root_url
+    end
+  end
+
+  def index
+    @categories = Category.all.order(name: :asc)
+  end
+
+  private
+
+  def category_params
+      params.require(:category).permit(:name)
+  end
+end
